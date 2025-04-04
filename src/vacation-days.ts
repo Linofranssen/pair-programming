51b1ab363b528f4d6920a35320f46c5ef818dd89
Noah Krummenacher <krummenachernoah@outlook.com>
2025-04-04 13:37:08 +0200

    Finishing Task 0 and 1

--- src/vacation-days.ts
+++ src/vacation-days.ts
@@ -6,6 +6,22 @@ export type Employment = {
 };
 
 export function calculateProRataVacationDays(employment: Employment): number {
-  // TODO: calculate pro rata (consider workload and days worked)
-  return employment.vacationDays;
+  const { startDate, untilDate, percentage, vacationDays } = employment;
+
+  if (startDate.getFullYear() === untilDate.getFullYear() &&
+      startDate.getMonth() === 0 && startDate.getDate() === 1 &&
+      untilDate.getMonth() === 11 && untilDate.getDate() === 31 &&
+      percentage === 100) {
+    return vacationDays; // Vollzeit über das ganze Jahr
+  }
+
+  const daysInYear = 365;
+  const daysWorked = calculateDaysBetween(startDate, untilDate);
+
+  return (vacationDays * daysWorked / daysInYear) * (percentage / 100);
+}
+
+function calculateDaysBetween(startDate: Date, endDate: Date): number {
+  const oneDay = 24 * 60 * 60 * 1000; // Stunden*Minuten*Sekunden*Millisekunden
+  return Math.round(Math.abs((endDate.getTime() - startDate.getTime()) / oneDay)) + 1; // + 1 da beide Tage mitgerechnet werden sollen.
 }
